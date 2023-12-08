c54803b4a0c7c923c945904a9385ec77f27b2a7b
Tom Horn <tom.horn@gmail.com>
2023-12-08 22:38:15 +1100

    Day eight.

--- day-eight.clj
+++ day-eight.clj
@@ -4,7 +4,7 @@
 (require '[clojure.set :as set])
 (require '[clojure.math :refer [ceil floor round pow]])
 
-(def test-data-raw ["RL"
+(def part-one-test-data-raw ["RL"
                     ""
                     "AAA = (BBB, CCC)"
                     "BBB = (DDD, EEE)"
@@ -14,6 +14,17 @@
                     "GGG = (GGG, GGG)"
                     "ZZZ = (ZZZ, ZZZ)"
                     ])
+
+(def test-data-raw ["LR"
+  ""
+  "11A = (11B, XXX)"
+  "11B = (XXX, 11Z)"
+  "11Z = (11B, XXX)"
+  "22A = (22B, XXX)"
+  "22B = (22C, 22C)"
+  "22C = (22Z, 22Z)"
+  "22Z = (22B, 22B)"
+  "XXX = (XXX, XXX)"])
 
 (def real-data-raw (str/split-lines (slurp "day-eight.txt")))
 
@@ -31,11 +42,59 @@
 (defn index-for-direction [instruction]
   (case instruction \L 0 \R 1))
 
-(defn part-one [data]
+(defn steps-between-nodes [data from is-finished?]
   (let [[instructions tree] (parse-instructions-and-tree data)]
     (loop [steps 0
-           current-root "AAA"]
-      (if (= current-root "ZZZ")
+           current-root from]
+      (if (is-finished? current-root)
         steps
         (let [root-branches (tree current-root)]
           (recur (inc steps) (nth root-branches (index-for-direction (instruction-at-index instructions steps)))))))))
+
+;; part one
+(defn part-one [data]
+  (steps-between-nodes data "AAA" (fn [node] (= node "ZZZ"))))
+
+(defn nodes-ending-in [tree ending]
+  (filter (fn [node] (= (last node) ending)) (keys tree)))
+
+(defn node-ending-in? [node ending]
+  (= (last node) ending))
+
+(defn are-nodes-ending-in? [nodes ending]
+  (every? (fn [node] (node-ending-in? node ending)) nodes))
+
+(defn next-roots-for-index-and-current-roots [instructions tree steps current-roots]
+  (doall (map (fn [current-root]
+                (nth (tree current-root)
+                     (index-for-direction (instruction-at-index instructions steps)))) current-roots)))
+
+(def memo-next-roots-for-index-and-current-roots (memoize next-roots-for-index-and-current-roots))
+
+(defn part-two-brute-force [data]
+  ;; brute force? nope
+  (let [[instructions tree] (parse-instructions-and-tree data)]
+    (loop [steps 0
+           current-roots (nodes-ending-in tree \A)]
+      (if (are-nodes-ending-in? current-roots \Z)
+        steps
+        (do
+          (recur (inc steps) (memo-next-roots-for-index-and-current-roots instructions tree steps current-roots)))))))
+
+
+(defn gcd [a b]
+  (if (zero? b)
+    a
+    (recur b (mod a b))))
+
+(defn lcm [a b]
+  (/ (* a b) (gcd a b)))
+
+(defn lcm-seq [numbers]
+  (reduce lcm numbers))
+
+(defn part-two [data]
+  (let [[instructions tree] (parse-instructions-and-tree data)
+        ;; get a number of steps for each ..A -> ..Z
+        all-steps (map (fn [from] (steps-between-nodes data from (fn [node] (node-ending-in? node \Z)))) (nodes-ending-in tree \A))]
+    (lcm-seq all-steps)))
