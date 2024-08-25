e3a4a5ad5f6d2e959b7cf6eaeab1371333bf70c8
Tom Horn <tom.horn@gmail.com>
2024-08-25 15:33:58 +1000

    Day twelve. Working recursive solution.
    No memory of what I was doing with day-five?

--- day-five.clj
+++ day-five.clj
@@ -40,26 +40,71 @@
 
 (def real-data-raw (str/split-lines (slurp "day-five.txt")))
 
-(defn check-mapping [mapping value]
-  (let [[to-start from-start length] mapping]
+(defn check-mapping [mapping value reverse-map]
+  (let [[to-start from-start length] (if reverse-map (reverse mapping) mapping)]
     (if (and (>= value from-start) (< value (+ from-start length)))
       ;; it's a match
       (+ to-start (- value from-start))
       ;; no match, so nil
       nil)))
 
-(defn map-or-return [mappings value]
+(defn map-or-return [mappings value reverse-map]
   ;; ok wait. If we use regular dictionaries, ie, this code
   ;; (or (dictionary value) value)
   ;; things get out of hand with the very big ranges in the real data.
   ;; So instead let's just lookat the from and to info
   (or (loop [i 0 maybe-found-mapping nil]
-     (if (or (= i (count mappings)) maybe-found-mapping)
-      ;; we found a mapping or ran out of mappings, so return the thing we maybe
-      ;; found.
-       maybe-found-mapping
-      ;; otherwise, keep looking
-       (recur (inc i) (check-mapping (nth mappings i) value)))) value))
+        (if (or (= i (count mappings)) maybe-found-mapping)
+          ;; we found a mapping or ran out of mappings, so return the thing we maybe
+          ;; found.
+          maybe-found-mapping
+          ;; otherwise, keep looking
+          (recur (inc i) (check-mapping (nth mappings i) value reverse-map)))) value))
+
+(defn reverse-map-or-return [mappings value]
+  (map-or-return mappings value true))
+
+(defn forward-map-or-return [mappings value]
+  (map-or-return mappings value false))
+
+(defn map-seed-number-location [almanac seed-number-or-location reverse-map]
+  ;; an almanac is a series of dictionaries that map values
+  (let [{seed-to-soil :seed-to-soil
+         soil-to-fertilizer :soil-to-fertilizer
+         fertilizer-to-water :fertilizer-to-water
+         water-to-light :water-to-light
+         light-to-temperature :light-to-temperature
+         temperature-to-humidity :temperature-to-humidity
+         humidity-to-location :humidity-to-location} almanac
+        forward-mappings [seed-to-soil
+                          soil-to-fertilizer
+                          fertilizer-to-water
+                          water-to-light
+                          light-to-temperature
+                          temperature-to-humidity
+                          humidity-to-location]
+        reverse-mappings (reverse forward-mappings)
+        mappings (if reverse-map reverse-mappings forward-mappings)]
+    (println mappings)
+    (reduce (fn [number mapping]
+              (map-or-return mapping number reverse-map)) seed-number-or-location mappings)))
+
+(defn map-location-to-seed-number [almanac location]
+  (let [{soil-to-seed :soil-to-seed
+         fertilizer-to-soil :fertilizer-to-soil
+         water-to-fertilizer :water-to-fertilizer
+         light-to-water :light-to-water
+         temperature-to-light :temperature-to-light
+         humidity-to-temperature :humidity-to-temperature
+         location-to-humidity :location-to-humidity} almanac]
+    (->> location
+         (reverse-map-or-return location-to-humidity)
+         (reverse-map-or-return humidity-to-temperature)
+         (reverse-map-or-return temperature-to-light)
+         (reverse-map-or-return light-to-water)
+         (reverse-map-or-return water-to-fertilizer)
+         (reverse-map-or-return fertilizer-to-soil)
+         (reverse-map-or-return soil-to-seed))))
 
 (defn map-seed-number-to-location [almanac seed-number]
   ;; an almanac is a series of dictionaries that map values
@@ -71,23 +116,36 @@
          temperature-to-humidity :temperature-to-humidity
          humidity-to-location :humidity-to-location} almanac]
     (->> seed-number
-         (map-or-return seed-to-soil)
-         (map-or-return soil-to-fertilizer)
-         (map-or-return fertilizer-to-water)
-         (map-or-return water-to-light)
-         (map-or-return light-to-temperature)
-         (map-or-return temperature-to-humidity)
-         (map-or-return humidity-to-location))))
+         (forward-map-or-return seed-to-soil)
+         (forward-map-or-return soil-to-fertilizer)
+         (forward-map-or-return fertilizer-to-water)
+         (forward-map-or-return water-to-light)
+         (forward-map-or-return light-to-temperature)
+         (forward-map-or-return temperature-to-humidity)
+         (forward-map-or-return humidity-to-location))))
 
-(def map-seed-number-to-location-memo (memoize map-seed-number-to-location))
+(let [[seeds almanac] (parse-data real-data-raw)]
+  (map-seed-number-location almanac 1 false))
 
-(assert (= (map-seed-number-to-location {:seed-to-soil [[3 1 1]]
+(let [[seeds almanac] (parse-data real-data-raw)]
+  (map-seed-number-to-location almanac 1))
+
+(let [[seeds almanac] (parse-data real-data-raw)]
+  (map-location-to-seed-number almanac 1))
+
+(let [[seeds almanac] (parse-data real-data-raw)]
+  (map-seed-number-location almanac 1 true))
+
+
+(def map-seed-number-location-memo (memoize map-seed-number-location))
+
+(assert (= (map-seed-number-location {:seed-to-soil [[3 1 1]]
                                          :soil-to-fertilizer []
                                          :fertilizer-to-water []
                                          :water-to-light []
                                          :light-to-temperature []
                                          :temperature-to-humidity []
-                                         :humidity-to-location []} 1) 3))
+                                         :humidity-to-location []} 1 false) 3))
 
 (defn parse-numbers-str [numbers-str]
   (map
@@ -122,12 +180,7 @@
 
 (defn part-one [data]
   (let [[seeds almanac] (parse-data data)
-        mapped-seeds (map (fn [seed-number] (map-seed-number-to-location-memo almanac seed-number)) seeds)]
-    (apply min mapped-seeds)))
-
-(defn part-one [data]
-  (let [[seeds almanac] (parse-data data)
-        mapped-seeds (map (fn [seed-number] (map-seed-number-to-location-memo almanac seed-number)) seeds)]
+        mapped-seeds (map (fn [seed-number] (map-seed-number-location-memo almanac seed-number false)) seeds)]
     (apply min mapped-seeds)))
 
 (defn part-two-test [data]
@@ -140,24 +193,47 @@
        (take (/ count step))))
 
 (defn map-seed-numbers-to-min-location [almanac [start count]]
-  (apply min (map (fn [index]
-                    (if (= (mod index 1000000) 0)
-                      (println (format "%f" (float (/ index count)))))
-                    (map-seed-number-to-location-memo almanac (+ start index)))
-                  (stepped-range 0 count (int (/ count 10000))))))
+  ;; if the count is big we used a stepped range to check (ie big gaps in the
+  ;; data) to find a lowest location. Once we have that we are going to scan
+  ;; downwards to see if there are any lower viable locations (which there
+  ;; probably will be)
+  (let [range-to-check (if (> count 10000)
+                         (stepped-range 0 count (int (/ count 100000)))
+                         (range 0 count))]
+    (apply min (map (fn [index]
+                      (map-seed-number-location-memo almanac (+ start index) false))
+                    range-to-check))))
+
+(defn part-two-check-seed-fn [seeds location seed-number-to-check]
+  ;; is the location one of the seeds (just a list of seed numbers)
+  (let [partitioned-seeds (partition 2 seeds)]
+    (if (some (fn [[start count]]
+                (println seed-number-to-check)
+                (println start count)
+                (and (>= seed-number-to-check start) (< seed-number-to-check (+ start count)))) partitioned-seeds)
+      location
+      nil)))
+
+(defn scan-for-lower-locations [seeds almanac starting-location]
+  (loop [current-location starting-location
+         locations []]
+      ;; check a few thousand lower locations to see if we can find a lower
+      ;; location (based on our sampled lowest location)
+    (if (< current-location (- starting-location 100))
+      locations
+      (recur (dec current-location) (let [maybe-location (part-two-check-seed-fn
+                                                          seeds
+                                                          current-location
+                                                          (map-seed-number-location-memo almanac current-location true))]
+                                      (if maybe-location (conj locations maybe-location) locations))))))
 
 (defn part-two [data]
-  (let [[seeds almanac] (parse-data data)
-        partitioned-seeds (partition 2 seeds)]
-    (apply min (map (fn [partitioned-seed]
-                      (map-seed-numbers-to-min-location almanac partitioned-seed))
-                    partitioned-seeds))))
-
-(defn part-two-threaded [data]
   (let [[seeds almanac] (parse-data data)
         partitioned-seeds (partition 2 seeds)
         futures (map (fn [partitioned-seed]
                        (future (map-seed-numbers-to-min-location almanac partitioned-seed)))
-                     partitioned-seeds)]
-    (println (format "Blocking on %d futures" (count futures)))
-    (apply min (map deref futures))))
+                     partitioned-seeds)
+        maybe-lowest-location (apply min (map deref futures))]
+    maybe-lowest-location))
+
+
--- day-twelve.clj
+++ day-twelve.clj
@@ -47,8 +47,10 @@
     (map (comp replace-binary #(format binary-format (Integer/toBinaryString %)))
          (range max-value))))
 
+(def generate-custom-binary-sequences-memo (memoize generate-custom-binary-sequences))
+
 (defn spring-combinations [spring-counts]
-  (generate-custom-binary-sequences spring-counts [\. \#]))
+  (generate-custom-binary-sequences-memo spring-counts [\. \#]))
 
 (defn all-arrangements [springs spring-unknowns]
   ;; for each of the spring unknown indexes cycle through \. and \#
@@ -58,6 +60,8 @@
                      (replace-at new-springs unknown (nth combination i)))
                    springs (map-indexed (fn [i v] [i v]) spring-unknowns))) combinations)))
 
+(def all-arrangements-memo (memoize all-arrangements))
+
 (defn failed-spring-groups-for-arrangement [arrangement]
   ;; \# are failed springs. How are they grouped contiguously?
   (map count (filter #(= (first %) \#) (partition-by #(= % \#) arrangement))))
@@ -66,11 +70,97 @@
   (= (failed-spring-groups-for-arrangement arrangement) groups))
 
 (defn valid-arrangements [[springs spring-unknowns groups]]
-  (let [arrangements (all-arrangements springs spring-unknowns)]
+  (let [arrangements (all-arrangements-memo springs spring-unknowns)]
     (filter (fn [arrangement] (valid-arrangement arrangement groups)) arrangements)))
 
 (defn valid-arrangements-count [[springs spring-unknowns groups]]
   (count (set (valid-arrangements [springs spring-unknowns groups]))))
 
-(defn part-one [data]
+(defn solve-v1 [data]
   (reduce + (map valid-arrangements-count (read-data data))))
+
+(defn part-one [data]
+  (solve-v1 data))
+
+(defn update-for-part-two [line]
+  (let [multiple 5
+        [springs groups] (str/split line #" ")
+        repeated-springs (take multiple (repeat springs))
+        repeated-groups (take multiple (repeat groups))]
+    (str (str/join "?" repeated-springs) " " (str/join "," repeated-groups))))
+
+;; So, this row:
+
+;; .# 1
+
+;; Would become:
+
+;; .#?.#?.#?.#?.# 1,1,1,1,1
+
+(assert (= (update-for-part-two ".# 1") ".#?.#?.#?.#?.# 1,1,1,1,1"))
+
+(defn options-for-spring-status [spring-status]
+  (case spring-status
+    ;; Operational
+    \. [\.]
+    ;; Fail
+    \# [\#]
+    ;; Unknown
+    \? [\# \.]))
+
+(defn valid-springs-given-groups [current-contiguous-failed-springs active-failed-spring-group remaining-groups]
+  (if (empty? remaining-groups)
+    ;; if there are no failed spring groups left, then we can only have operational springs
+    #{\.}
+    (if (not active-failed-spring-group)
+      ;; if the current failed group isn't set, then we can have either type (an
+      ;; operation spring won't change anything and a failed spring will start
+      ;; the next group)
+      #{\. \#}
+      (if (< current-contiguous-failed-springs (first remaining-groups))
+        ;; if the current contiguous failed springs is less than the current
+        ;; groups maximum contiguous failed springs, then we can add a failed
+        ;; spring and still be valid
+        #{\#}
+        ;; if the current contiguous-failed-springs is equal to the current
+        ;; group, then all we can have is an operational spring next
+        #{\.}))))
+
+(defn recurse-valid-arrangements [springs current-contiguous-failed-springs active-failed-spring-group remaining-groups]
+  (if (empty? springs)
+    ;; we checked the entire string and there were no remaining groups we can return 1 (ie a valid path)
+    (do
+      ;; (println "We got to the end of a path and remaining groups were " remaining-groups " and contiguous-failed-springs was " current-contiguous-failed-springs)
+      (if (or (empty? remaining-groups) (and (= (count remaining-groups) 1) (= (first remaining-groups) current-contiguous-failed-springs))) 1 0))
+    (let [valid-springs (valid-springs-given-groups current-contiguous-failed-springs active-failed-spring-group remaining-groups)
+          potential-springs-for-next-index (case (first springs)
+                                             ;; Operational
+                                             \. [\.]
+                                             ;; Fail
+                                             \# [\#]
+                                             ;; Unknown
+                                             \? [\# \.])
+          available-springs-for-next-index (filter valid-springs potential-springs-for-next-index)]
+      (if (empty? available-springs-for-next-index)
+        ;; If there are no available springs to pick, then we can't continue down
+        ;; this recursive path and just return zero
+        0
+        ;; otherwise we continue to recurse for each of the
+        ;; available-springs-for-next-index
+        (reduce + (map (fn [available-spring]
+                         (case available-spring
+                           ;; we are trying an operation spring, if previously
+                           ;; there was an active-failed-spring-group we need to
+                           ;; remove the previously active group
+                           \. ((memoize recurse-valid-arrangements) (rest springs) 0 false (if active-failed-spring-group (rest remaining-groups) remaining-groups))
+                           \# ((memoize recurse-valid-arrangements) (rest springs) (inc current-contiguous-failed-springs) true remaining-groups)))
+                       available-springs-for-next-index))))))
+
+(defn solve-last-v2 [data]
+  (reduce + (map (fn [[springs spring-unknowns groups]] (recurse-valid-arrangements springs 0 false groups)) [(last (read-data data))])))
+
+(defn solve-v2 [data]
+  (reduce + (map (fn [[springs spring-unknowns groups]] (recurse-valid-arrangements springs 0 false groups)) (read-data data))))
+
+(defn part-two [data]
+  (solve-v2 (map update-for-part-two data)))
